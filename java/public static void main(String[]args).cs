6809d3b281f8a06dcf51acaf7d33df4dbfdf7c7e
Savitskim <savitskim@inbox.ru>
2022-10-14 18:03:41 +0300

    Initial commit

---
+++ java/public static void main(String[]args).cs
@@ -0,0 +1,7 @@
+using System.Diagnostics;
+int start=1;
+while(start<5)
+{
+    Debug.WriteLine(start);
+    start+=1;
+}
---
+++ java/public static void main(String[]args).java
@@ -0,0 +1,7 @@
+public static void main(String[]args)
+integer input = 5;
+integer a=input+10;
+while(a<30){
+    a=a*2;
+}
+System.out.println(a);
