8fdfba0a1acbdabdaed1ef15205b90cf34d82d39
Savitskim <savitskim@inbox.ru>
2023-01-27 18:58:00 +0300

    gitignore

---
+++ Pitosha/example.py
@@ -0,0 +1,74 @@
+# print('Hello world')
+# value = None
+# a = 123
+# b = 1.23
+# print(type(a))
+# print(type(b))
+# value = 12334
+# print(type(value))
+# s = 'Hello world'
+# print(s)
+# print(a,b,s)
+# print(a,'-',b,'-',s)
+# print('{1} - {2} - {0}'.format(a,b,s))
+# print(f'{a} - {b} - {s}')
+# list = ['1','2','4','hello']
+# # print(list)
+# print('Введите а:')
+# a = int(input())
+# print('Введите b:')
+# b = int(input())
+# print(a, ' ', b, ' ', a+b)
+# a=1.3
+# b=3
+# c=round(a * b, 1)
+# print(c)
+# func = 1
+# t = 5
+# x = 123
+# print(func<x>t)
+# t = 1>2 or 3<4
+# print(t)
+# f = [1,2,3,4]
+# print(2 in f)
+# f=[1,2,3,4]
+# is_odd = f[1] % 2==0
+# print(is_odd)
+# a = int(input('a = '))
+# b = int(input('b = '))
+# if a > b:
+#  print(a)
+# else:
+#  print(b)
+# original = 23
+# inverted = 0
+# while original !=0:
+#     inverted = inverted*10+(original%10)
+#     original //=10
+# else:
+#     print('Пожалуй')
+#     print('хватит')
+# print(inverted)    
+# list = [1,2,3,4,5]
+# for i in list:
+#     print(i)
+# r = range(10)
+# for i in r:
+#     print(i+1)
+# for i in range(1, 10, 2):
+#     print(i)
+# text =  'съешь ещё этих мягких французских булок'
+
+# print(len(text))
+# print('ещё' in text)
+# print(text[:5] + text[9:14])
+
+# def f(x):
+#     if x ==1:
+#         return 'Целое'
+#     if x == 2.3:
+#         return 23
+#     else:
+#         return 
+# arg = 2
+# print(f(arg))
