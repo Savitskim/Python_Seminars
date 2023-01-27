de6f7b39c5024c2a7215cf9f10946888e394f537
Savitskim <savitskim@inbox.ru>
2023-01-27 23:05:34 +0300

    HomeWork1

---
+++ Pitosha/HomeTask2.py
@@ -0,0 +1,12 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+number = int(input("Введите трехзначное число: "))
+result = 0
+if number < 0:
+    number *= -1
+if number > 99 and number < 1000:
+    while number > 0:
+        result += number % 10
+        number //= 10
+    print(result)
+else:
+    print("Число не является трехзначным!")
